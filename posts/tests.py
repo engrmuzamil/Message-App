95840640c01126e3e6918026766bc56b95c003b0
Muzamilkhan-Waveit <muzamilkhan111814@gmail.com>
2021-12-11 20:12:00 +0500

    After Test Cases

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from .models import Post
+class postModeltest(TestCase):
+    def setUp(self):
+        Post.objects.create(message="Message")
 
-# Create your tests here.
+    def test_msg(self):
+        post = Post.objects.get(id = 1)
+        objectname = f'{post.message}'
+        self.assertEqual(objectname,"Message")
+
+from .views import homeView
+from django.urls import reverse
+class homeviewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(message="message")
+
+    def test_url_exists(self):
+        responseRecv  = self.client.get('/')
+        self.assertEqual(responseRecv.status_code, 200)
+    
+    def test_url_name(self):
+        responseRecv = self.client.get(reverse('home'))
+        self.assertEqual(responseRecv.status_code, 200)
+    
+    def test_View_Uses_Correct_Template(self):
+        reponseRecv= self.client.get(reverse('home'))
+        self.assertEqual(reponseRecv.status_code, 200)
+        self.assertTemplateUsed(reponseRecv, 'index.html')
+
+    
